94b484fb6037272faae3584d304e8fe0c229814b
George Britton <george.britton@student.nua.ac.uk>
2018-11-16 10:49:36 +0000

    Update

---
+++ Source/WeekOneProject/WeekOneProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WeekOneProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWeekOneProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AWeekOneProjectGameMode();
+};
+
+
+
